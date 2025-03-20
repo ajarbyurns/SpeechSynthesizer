4dd847c4a9b4af235783d74a2a284b738300dbda
BARRY JUANS <barry.juans@fintopia.tech>
2025-03-20 12:15:49 +0700

    Initial Commit

---
+++ SpeechSynthesis/SpeechSynthesisApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechSynthesisApp.swift
+//  SpeechSynthesis
+//
+//  Created by Barry Juans on 20/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SpeechSynthesisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
